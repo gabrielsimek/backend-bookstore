12220f76095cc9e68cc1bcbabb5ae4259ba10812
CaseyCameron <mcasey.cameron@gmail.com>
2022-03-09 16:42:34 -0800

    GET all

--- __tests__/author.test.js
+++ __tests__/author.test.js
@@ -24,4 +24,25 @@ describe('backend-bookstore routes', () => {
       .send(author);
     expect(res.body).toEqual({ ...author, id: expect.any(String) });
   });
+
+  it('should get all authors', async () => {
+    const authors = [
+      {
+        name: 'Murakami',
+        dob: '4/27/2019',
+        pob: 'Tokyo'
+      }, 
+      {
+        name: 'Hithcock',
+        dob: '4/27/1935',
+        pob: 'Los Angeles'
+      }, 
+    ];
+
+    const expected = await Promise.all(authors.map(author => Author.insert(author)));
+    const res = await request(app).get('/authors');
+
+    expect(res.body).toEqual(expect.arrayContaining(expected));
+    expect(res.body).toHaveLength(2);
+  });
 });
