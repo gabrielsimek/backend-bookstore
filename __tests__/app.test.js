7042ba7d38d376120c017bd50be9e60d554d78d2
Gabriel Simek <gabrielsimek@outlook.com>
2022-03-09 16:01:35 -0800

    add publisher MVC and test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Publisher = require('../lib/models/Publisher');
 
 describe('backend-bookstore routes', () => {
   beforeEach(() => {
@@ -10,5 +11,20 @@ describe('backend-bookstore routes', () => {
 
   afterAll(() => {
     pool.end();
+  });
+
+  it('posts a publisher', async () => {
+    const publisher = {
+      name: 'Penguin',
+      city: 'New York',
+      state: 'New York',
+      country: 'United States',
+    };
+    const res = await request(app).post('/publishers').send(publisher);
+
+    expect(res.body).toEqual({
+      ...publisher,
+      id: expect.any(String),
+    });
   });
 });
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,12 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS publisher CASCADE;
+
+CREATE TABLE publisher (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    city VARCHAR(255),
+    state VARCHAR(255),
+    country VARCHAR(255)
+);
+
