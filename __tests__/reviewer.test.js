102308db23d7e935a6c0a0bd343991c911be9602
Gabriel Simek <gabrielsimek@outlook.com>
2022-03-09 17:03:47 -0800

    GET all reviewer

--- __tests__/reviewer.test.js
+++ __tests__/reviewer.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Reviewer = require('../lib/models/Reviewer');
 
 describe('backend-bookstore routes', () => {
   beforeEach(() => {
@@ -24,5 +25,26 @@ describe('backend-bookstore routes', () => {
       ...reviewer,
       id: expect.any(String),
     });
+  });
+
+  it('gets all reviewers', async () => {
+    const reviewers = [
+      {
+        name: 'Roger Ebert',
+        company: 'Siskel and Ebert',
+      },
+      {
+        name: 'Casey Cameron',
+        company: 'Alchemy Code Labs',
+      },
+    ];
+
+    const expected = await Promise.all(
+      reviewers.map((reviewer) => Reviewer.insert(reviewer))
+    );
+
+    const res = await request(app).get('/reviewers');
+
+    expect(res.body).toEqual(expect.arrayContaining(expected));
   });
 });
